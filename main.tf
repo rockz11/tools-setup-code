455daf3c9d3a847e62358db1854243e0476b8680
Prughvi <prudhviraj3525@gmail.com>
2024-12-11 12:19:12 +0530

    m

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "tools" {
-  for_each = var.tools
-  source = "./module-ec2"
-  tool_name = each.key
-  sg_port = each.value["port"]
+  for_each    = var.tools
+  source      = "./module-ec2"
+  tool_name   = each.key
+  sg_port     = each.value["port"]
   volume_size = each.value["volume_size"]
   domain_name = var.domain_name
-  zone_id = var.zone_id
+  zone_id     = var.zone_id
+
+}
