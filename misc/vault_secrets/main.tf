04d2be7330ba8ed37ad36c9529a353023022ee96
Prughvi <prudhviraj3525@gmail.com>
2024-12-20 21:29:15 +0530

    vault

---
+++ misc/vault_secrets/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform143"
+    key    = "vault-secrets/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "https://vault-internal.devops11.online:8200"
+  token = var.vault_token
+  skip_tls_verify = true
+}
+variable "vault_token" {}
+
+resource "vault_generic_secret" "roboshop-dev" {
+  path      = "roboshop-dev/frontend"
+  data_json = <<EOT
+{
+  "foo": "bar",
+  "pizza": "cheese
+}
+EOT
+}
