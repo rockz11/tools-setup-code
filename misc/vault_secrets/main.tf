ca474486f21dd1af0540272b35528f43b9208018
Prughvi <prudhviraj3525@gmail.com>
2024-12-22 23:05:25 +0530

    m

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -1,16 +1,16 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform143"
-    key    = "vault-secrets/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-provider "vault" {
-  address = "http://vault-internal.devops11.online:8200"
-  token = var.vault_token
-  skip_tls_verify = true
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform143"
+#     key    = "vault-secrets/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+#
+# provider "vault" {
+#   address = "http://vault-internal.devops11.online:8200"
+#   token = var.vault_token
+#   skip_tls_verify = true
+# }
 
 variable "vault_token" {}
 
