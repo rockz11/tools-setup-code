84bf114d523c016625eb06648ce9b3aa71a01736
Prughvi <prudhviraj3525@gmail.com>
2024-12-23 21:04:16 +0530

    m

--- module-ec2/variables.tf
+++ module-ec2/variables.tf
@@ -9,8 +9,8 @@ tool_name = each.key
 sg_port = each.value["port"]
 volume_size = each.value["volume_size"]
 
-variable "tool_name" {}
-variable "sg_port" {}
-variable "volume_size" {}
+# variable "tool_name" {}
+# variable "sg_port" {}
+# variable "volume_size" {}
 
 variable "ami" {}
