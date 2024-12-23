a01e7c5185b60d47ea5c60304c5c50085ab04673
Prughvi <prudhviraj3525@gmail.com>
2024-12-23 21:01:30 +0530

    m

--- module-ec2/variables.tf
+++ module-ec2/variables.tf
@@ -1,16 +1,16 @@
-# variable "env" {}
-# variable "component_name" {}
+variable "env" {}
+variable "component_name" {}
 variable "instance_type" {}
-# variable "app_port" {}
+variable "app_port" {}
 variable "zone_id" {}
 variable "domain_name" {}
 
-#tool_name = each.key
-#sg_port = each.value["port"]
-#volume_size = each.value["volume_size"]
+tool_name = each.key
+sg_port = each.value["port"]
+volume_size = each.value["volume_size"]
 
 variable "tool_name" {}
 variable "sg_port" {}
 variable "volume_size" {}
 
-# variable "ami" {}
+variable "ami" {}
