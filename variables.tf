a9568392d539d4b71bb83694596ad1e60f98307e
Prughvi <prudhviraj3525@gmail.com>
2024-12-24 23:01:42 +0530

    m

--- variables.tf
+++ variables.tf
@@ -2,16 +2,28 @@ variable "tools" {
   default = {
 
     vault = {
-      port        = 8200
-      volume_size = 20
+      port          = 8200
+      volume_size   = 20
       instance_type = "t3.small"
+      policy_list = []
     }
 
 
     github-runner = {
-      port        = 80 # Just a dummy port , it wont open any port.
-      volume_size = 20
+      port = 80 # Just a dummy port , it wont open any port.
+      volume_size   = 20
       instance_type = "t3.small"
+      policy_list = ["*"]
+    }
+    elasticsearch = {
+      port = {
+        elasticsearch = 9200
+        nginx         = 80
+        logstash      = 5044
+      }
+      volume_size   = 50
+      instance_type = "r6idn.large"
+      policy_list = []
     }
   }
 }
